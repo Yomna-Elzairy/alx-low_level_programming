96f7256260804432f4adfeb3142425a36193b7c5
yomnaelzairy <yomnaelzairy@gmail.com>
2023-07-15 05:26:42 +0300

    test

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -7,9 +7,11 @@
  */
 int main(void)
 {
-    for (char ch = "a"; ch <= z; ch++)
+    char alph[26] = "abcdefghijklmnopqrstvwxyz";
+    int i;
+    for (i = 0; i < 26; i++)
     {
-        putchar(ch);
+        putchar(alph[i]);
     }
     putchar('\n');
    	return (0);
