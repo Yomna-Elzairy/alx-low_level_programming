17700fae325a1df758b34d0b860e1bf723457c3a
yomnaelzairy <yomnaelzairy@gmail.com>
2023-07-21 01:42:33 +0300

    test

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -5,12 +5,13 @@
  *
  * Return: Always 0 (Success)
  */
- void print_alphabet(void)
- {
-    int i;
-    for (i = 97; i <= 122; i++)
-    {
-        _putchar(i);
-    }
-    _putchar('\n');
- }
+void print_alphabet(void)
+{
+	int i;
+
+	for (i = 97; i <= 122; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}
